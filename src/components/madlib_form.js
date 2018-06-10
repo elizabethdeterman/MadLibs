7cc68c10971d0a297e2d5464a825f48b84a9dd4c
Liz Determan <edeterman@dawson.edu>
2018-06-10 14:48:45 -0600

    Added react strap and inputs

--- src/components/madlib_form.js
+++ src/components/madlib_form.js
@@ -1,9 +1,60 @@
 import React, { component } from 'react';
-
+import {
+    Col,
+    Row    
+}
+from 'reactstrap'; 
 
 class MadlibForm extends React.Component {
+    handleChange = function() {
+                console.log('trying to handle change');
+            }
     render() {
-      return <h1>Hello, {this.props.name}</h1>;
+      return (
+          <Row style={{textAlign:'center', color:'white'}}>
+              <Col md="3" className="inputWrapper">
+              <Row>
+                  <Col md="2">
+                    <lable className="greenlable">1</lable>
+                  </Col>
+                  <Col md="10">
+                  <input placeholder='Color' type='text' onChange={this.handleChange}/>
+                  </Col>
+              </Row>
+              </Col>
+              <Col md="3" className="inputWrapper">
+              <Row>
+                  <Col md="2">
+                    <lable className="greenlable">1</lable>
+                  </Col>
+                  <Col md="10">
+                  <input placeholder='Color' type='text' onChange={this.handleChange}/>
+                  </Col>
+              </Row>
+              </Col>
+              <Col md="3" className="inputWrapper">
+              <Row>
+                  <Col md="2">
+                    <lable className="greenlable">1</lable>
+                  </Col>
+                  <Col md="10">
+                  <input placeholder='Color' type='text' onChange={this.handleChange}/>
+                  </Col>
+              </Row>
+              </Col>
+              <Col md="3" className="inputWrapper">
+              <Row>
+                  <Col md="2">
+                    <lable className="greenlable">1</lable>
+                  </Col>
+                  <Col md="10">
+                  <input placeholder='Color' type='text' onChange={this.handleChange}/>
+                  </Col>
+              </Row>
+              </Col>
+          </Row>
+
+      );
     }
   }
 
